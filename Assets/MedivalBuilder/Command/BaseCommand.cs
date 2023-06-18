7e53ab3d123c40aead3b7c33549d5a7cc719dbdf
BerezovskiAlex <berezovskiy_aleksey@smartworld.team>
2023-06-18 15:44:18 +0700

    Add commands realization

---
+++ Assets/MedivalBuilder/Command/BaseCommand.cs
@@ -0,0 +1,10 @@
+﻿using System;
+
+namespace MedivalBuilder.Command
+{
+    public abstract class BaseCommand : ICommand
+    {
+        public abstract event Action OnDone;
+        public abstract void Do();
+    }
+}
---
+++ Assets/MedivalBuilder/Command/ICommand.cs
@@ -0,0 +1,10 @@
+﻿using System;
+
+namespace MedivalBuilder.Command
+{
+    public interface ICommand
+    {
+        event Action OnDone;
+        void Do();
+    }
+}
